2fa954fad1281b796696665b40e0599d232a4b91
marymota <dg.marciamota@gmail.com>
2021-03-01 11:58:03 +0000

    Completed Part2

--- libft/ft_itoa.c
+++ libft/ft_itoa.c
@@ -1,43 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marmota <marmota@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/28 22:02:47 by marmota           #+#    #+#             */
+/*   Updated: 2021/02/28 22:02:49 by marmota          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
-char    *ft_itoa(int n)
+static int	count_digits(int n)
 {
-    char *ret;
-    int i;
-    int j;
-    int sign;
-    
-    if (n < 0)
-    {
-      n = n * -1;
-      sign = 1;
-    }
-    else
-    {
-      sign = 0;
-    }
-    if (n > 0)
-    {
-      i = n;
-      j = 0;
-      while (i > 0)
-      {
-        i = i / 10;
-        ++j;
-      }
-      ret = malloc(sizeof(char) * (j + 1 + sign));
-      ret[j] = 0;
-    }
-    while (n >= 10)
-    {
-        ret[--j] = (n % 10) + '0'; 
-        n = n / 10;
-    }
-    if (n < 10)
-    {
-        ret[--j] = n + '0';
-    }
-    if (sign > 0)
-        ret[0] = '-';
-    return (ret);
+	int			i;
+
+	i = 0;
+	while (n / 10)
+	{
+		++i;
+		n = n / 10;
+	}
+	++i;
+	return (i);
+}
+
+static int	negative_numbers(int num)
+{
+	if (num < 0)
+	{
+		num = num * -1;
+		return (1);
+	}
+	return (0);
+}
+
+char	*ft_itoa(int n)
+{
+	char		*ret;
+	int			i;
+	int			sign;
+	long int	num;
+
+	num = n;
+	sign = negative_numbers(num);
+	if (sign)
+		num = num * -1;
+	i = count_digits(num) + sign;
+	ret = malloc(sizeof(char) * i + 1);
+	if (!ret)
+		return (0);
+	ret[i] = 0;
+	while (num >= 10)
+	{
+		ret[--i] = (num % 10) + '0';
+		num = num / 10;
+	}
+	ret[--i] = num + '0';
+	if (sign > 0)
+		ret[0] = '-';
+	return (ret);
 }
